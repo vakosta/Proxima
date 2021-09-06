a9e1b2932f1a5b4ba9800bda4910ce2e2898fb60
Vakosta <v.a.annenkov@ya.ru>
2021-09-06 18:07:20 +0300

    feat: add base files

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.5.10"
     id("org.jetbrains.compose") version "0.4.0"
 }
 
---
+++ src/main/kotlin/presentation/windows/MainWindow.kt
@@ -0,0 +1,36 @@
+package presentation.windows
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MainWindow() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Column {
+            Button(onClick = {
+                text = "Hello, Desktop!"
+            }) {
+                Text(text)
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(onClick = {
+                text = "Hello, Desktop!"
+            }) {
+                Text(text)
+            }
+        }
+    }
+}
