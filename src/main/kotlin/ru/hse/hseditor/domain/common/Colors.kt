3a9287caadba0b1455bd0da23fe0ec91498612b9
Vakosta <v.a.annenkov@ya.ru>
2021-11-09 18:10:06 +0300

    fix: correct colors

--- src/main/kotlin/ru/hse/hseditor/domain/common/Colors.kt
+++ src/main/kotlin/ru/hse/hseditor/domain/common/Colors.kt
@@ -8,23 +8,30 @@ val COLOR_GRAY = Color.decode("#c0c0c0").rgb
 val COLOR_BLUE = Color.decode("#a6d2ff").rgb
 
 val COLOR_SPECIAL = Color.decode("#0033b3").rgb
+val COLOR_ANNOTATION = Color.decode("#af9a00").rgb
 val COLOR_NUMBER = Color.decode("#1d55ec").rgb
 val COLOR_STRING = Color.decode("#007719").rgb
 val COLOR_COMMENT = Color.decode("#afbdb2").rgb
 
 val TOKEN_COLORS = hashMapOf(
-    KotlinLexer.CLASS to COLOR_SPECIAL,  // CLASS
-    KotlinLexer.FUN to COLOR_SPECIAL,  // FUN
-    KotlinLexer.VAL to COLOR_SPECIAL,  // VAL
-    KotlinLexer.VAR to COLOR_SPECIAL,  // VAR
-    KotlinLexer.ELSE to COLOR_SPECIAL,  // ELSE
-    KotlinLexer.WHEN to COLOR_SPECIAL,  // WHEN
-    KotlinLexer.RETURN to COLOR_SPECIAL,  // RETURN
-    KotlinLexer.LATEINIT to COLOR_SPECIAL, // LATEINIT
+    KotlinLexer.CLASS to COLOR_SPECIAL,
+    KotlinLexer.FUN to COLOR_SPECIAL,
+    KotlinLexer.VAL to COLOR_SPECIAL,
+    KotlinLexer.VAR to COLOR_SPECIAL,
+    KotlinLexer.IF to COLOR_SPECIAL,
+    KotlinLexer.ELSE to COLOR_SPECIAL,
+    KotlinLexer.TRY to COLOR_SPECIAL,
+    KotlinLexer.CATCH to COLOR_SPECIAL,
+    KotlinLexer.PRIVATE to COLOR_SPECIAL,
+    KotlinLexer.PUBLIC to COLOR_SPECIAL,
+    KotlinLexer.WHEN to COLOR_SPECIAL,
+    KotlinLexer.RETURN to COLOR_SPECIAL,
+    KotlinLexer.LATEINIT to COLOR_SPECIAL,
     KotlinLexer.IMPORT to COLOR_SPECIAL,
     KotlinLexer.INTERFACE to COLOR_SPECIAL,
     KotlinLexer.OBJECT to COLOR_SPECIAL,
     KotlinLexer.PACKAGE to COLOR_SPECIAL,
+    KotlinLexer.ANNOTATION to COLOR_ANNOTATION,
 
     KotlinLexer.FloatLiteral to COLOR_NUMBER,  // FLOAT
     KotlinLexer.DoubleLiteral to COLOR_NUMBER,  // DOUBLE
